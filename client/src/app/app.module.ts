19865455334aafef16c24813f7b6f295ddf8ea72
jakubchmielewskiIS <jakub.chmielewski@innovationsoftware.pl>
2022-11-29 20:40:19 +0100

    move header to own layout directory

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { HeadingComponent } from './components/heading/heading.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomepageComponent, SignUpComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    SignUpComponent,
+    HeadingComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
