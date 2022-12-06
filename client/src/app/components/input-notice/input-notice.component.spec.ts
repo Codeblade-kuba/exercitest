d361ac48e8def4a3c82144a3759a419fd414fb5a
jakubchmielewskiIS <jakub.chmielewski@innovationsoftware.pl>
2022-12-06 19:25:37 +0100

    create input notice

---
+++ client/src/app/components/input-notice/input-notice.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputNoticeComponent } from './input-notice.component';
+
+describe('InputNoticeComponent', () => {
+  let component: InputNoticeComponent;
+  let fixture: ComponentFixture<InputNoticeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InputNoticeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InputNoticeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/input-notice/input-notice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-notice',
+  templateUrl: './input-notice.component.html',
+  styleUrls: ['./input-notice.component.css']
+})
+export class InputNoticeComponent {
+
+}
