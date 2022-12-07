8b2a89fb6787b1b5072213e58b4a665cddbdd98a
jakubchmielewskiIS <jakub.chmielewski@innovationsoftware.pl>
2022-12-07 18:41:10 +0100

    add protected dashboard routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginActivateGuard } from './guards/login-activate.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'sign-up', component: SignUpComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [LoginActivateGuard],
+  },
 ];
 
 @NgModule({
