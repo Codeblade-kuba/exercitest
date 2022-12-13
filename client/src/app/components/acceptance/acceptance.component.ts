9a6ea687462829e1a70c7b70d71588e59a2630cb
jakubchmielewskiIS <jakub.chmielewski@innovationsoftware.pl>
2022-12-13 20:28:01 +0100

    remove unnecessary imports

--- client/src/app/components/acceptance/acceptance.component.ts
+++ client/src/app/components/acceptance/acceptance.component.ts
@@ -1,5 +1,5 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ValidationErrors } from '@angular/forms';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-acceptance',
